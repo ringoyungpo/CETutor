800461b253aae90058e7ecaad36aebbabb651b98
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-05-12 09:16:01 +0800

    change api from local to heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // API_BASE: 'https://cetutor.herokuapp.com/',
-  API_BASE: 'http://192.168.199.159:5000/',
+  API_BASE: 'https://cetutor.herokuapp.com/',
+  // API_BASE: 'http://192.168.199.159:5000/',
 }
